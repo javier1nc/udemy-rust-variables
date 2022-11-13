db00998d68cc59711e5b9cf4b6410cb174c1dace
javier1nc <code@javier1nc.com>
2022-11-12 19:07:09 -0600

    udated code

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,11 @@ const STARTING_MISSILES: i32 = 8;
 const  READY_AMOUNT: i32 = 2;
 
 fn main() {
-    let mut missiles= STARTING_MISSILES;
-    let ready = READY_AMOUNT;
+    let  (missiles, ready)= (STARTING_MISSILES,READY_AMOUNT);
 
     println!("Firing {} of my {}...", ready, missiles);
 
-    missiles = missiles - ready;
+   let missiles = missiles - ready;
 
     println!("{} missiles left", missiles);
-
 }
